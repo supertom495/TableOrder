bd8c79d06beca5d92ebbd2d382630f3b8bce941c
supertom495 <supertom495@gmail.com>
2020-04-15 12:38:35 +1000

    update setting file methods

--- api/database.py
+++ api/database.py
@@ -3,17 +3,18 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import yaml
 import os, sys
 import pymssql
 
-if not os.path.exists("./setting"):
+if not os.path.exists("./flask.yaml"):
 	sys.exit()
-with open(r"setting", 'r') as f:
-	database = f.readline()
-	storeName = f.readline()
+with open('flask.yaml') as f:
+	data = yaml.load(f, Loader=yaml.FullLoader)
+	if (len(data) != 5): sys.exit()
 
-storeName = storeName
-engine = create_engine('mssql+pymssql://' + database.strip(), convert_unicode=True)  # 创建数据库引擎( 当前目录下保存数据库文件)
+storeName = data.get("StoreName")
+engine = create_engine('mssql+pymssql://{}:{}@{}/{}'.format(data.get("Login"), data.get("Password"), data.get("ServerName"), data.get("DBName")), convert_unicode=True)  # 创建数据库引擎( 当前目录下保存数据库文件)
 db_session = scoped_session(sessionmaker(autocommit=False,
 										 autoflush=False,
 										 bind=engine))
