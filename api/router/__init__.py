374897bdc62ed4dc8a97391cd0b64a28bdfe131e
supertom495 <supertom495@gmail.com>
2020-06-12 11:45:50 +1000

    restructure file

--- api/router/__init__.py
+++ api/router/__init__.py
@@ -1,9 +1,11 @@
 from router.menu import menu_blueprint
 from router.staff import staff_blueprint
 from router.order import order_blueprint
+from router.raw import raw_blueprint
 
 blueprint = [
     menu_blueprint,
     staff_blueprint,
-    order_blueprint
+    order_blueprint,
+    raw_blueprint
 ]
