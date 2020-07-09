4719ec3c7238558b029abf4afb7cf6910068073c
supertom495 <supertom495@gmail.com>
2020-07-09 13:49:00 +1000

    port customization

--- api/run_server.py
+++ api/run_server.py
@@ -3,7 +3,8 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from flask_api import app
+from database import getPort
 
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5001)
+http_server.listen(getPort())
 IOLoop.instance().start()
