ffc6d80c34d2d78aed40211b13da0ff4922a7905
supertom495 <supertom495@gmail.com>
2020-09-04 16:54:45 +1000

    1.27

--- api/run_server.py
+++ api/run_server.py
@@ -7,19 +7,19 @@ from flask_apscheduler import APScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 
-from database import getPort, init_db
-
+from database import getPort, init_db, flaskConfig
 # scheduler = apscheduler.schedulers.blocking.BackgroundScheduler('apscheduler.job_defaults.max_instances': '2')
 
-job_defaults = {
-    'coalesce': False,
-    'max_instances': 3
-}
+if flaskConfig.get('PiselUrl') is not None and flaskConfig.get('PiselSecret') is not None:
+    job_defaults = {
+        'coalesce': False,
+        'max_instances': 3
+    }
+    scheduler = APScheduler(BackgroundScheduler(job_defaults=job_defaults, timezone=utc))
+    # scheduler = APScheduler(BackgroundScheduler(timezone="Asia/Shanghai"))
+    scheduler.init_app(app)
+    scheduler.start()
 
-scheduler = APScheduler(BackgroundScheduler(job_defaults=job_defaults, timezone=utc))
-# scheduler = APScheduler(BackgroundScheduler(timezone="Asia/Shanghai"))
-scheduler.init_app(app)
-scheduler.start()
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(getPort())
 init_db()
