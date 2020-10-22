5af2152750a2cd81f186b99abec9248330d41487
supertom495 <supertom495@gmail.com>
2020-10-22 11:48:21 +1100

    1.31b tag

--- api/router/raw.py
+++ api/router/raw.py
@@ -16,7 +16,7 @@ raw_blueprint = flask.Blueprint(
 
 @raw_blueprint.route('/', methods=['GET'])
 def home():
-    return "<h1>RPOS online order</h1><h3>Store name: {}  V:1.31b(beta)hotfix</h3><p>This site has API for self-ordering.</p>".format(
+    return "<h1>RPOS online order</h1><h3>Store name: {}  V:1.31b</h3><p>This site has API for self-ordering.</p>".format(
         flaskConfig.get('StoreName'))
 
 
